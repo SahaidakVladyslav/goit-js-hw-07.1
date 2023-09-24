e93e5e6863f3f76e4c82db8b4369d944e1f62508
Vladyslav Sahaidak <svg.231@gmail.com>
2023-09-24 20:58:42 +0200

    'tadada-da'

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// // Change code below this line
+const basicLightbox = require('basiclightbox');
 
-console.log(galleryItems);
+import * as basicLightbox from 'basiclightbox';
+
+
+
+const listEl = document.querySelector('.gallery')
+
+
+
+const typset = galleryItems.map(({ preview, original, description }) => { return `<li><img src="${preview}" alt="${description}"></li>` }).join('')
+const bigTypset = galleryItems.map(({ original, description }) => `<img src = "${original}" alt = "${description}">`);
+
+listEl.insertAdjacentHTML("beforeend", typset)
+
+
+
+document.querySelector('li').addEventListener('click', () => {
+    basicLightbox.create(bigTypset)
+})
