5686678bc664edf9b13c543ded646aac6cb2c042
Vladyslav Sahaidak <svg.231@gmail.com>
2023-09-26 05:23:50 +0200

    'workDone'

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,62 +5,24 @@ console.log(galleryItems);
 
 // // Change code below this line
 
-
 const listEl = document.querySelector('.gallery')
 
 const typset = galleryItems.map(({ preview, original, description }) => {
-    return `<li  style="margin: 20px;" class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      loading="lazy"
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>` }).join('');
+    return `
+<li style="margin: 20px;" class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img loading="lazy" class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`}).join('');
 
 listEl.insertAdjacentHTML("beforeend", typset)
 
-
-
-const modalOpen = (event) => {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')) {
-        console.log('NEpraça');
-        return;
-    }
-    console.log('praça');
-    const clickedImageAlt = event.target.getAttribute('alt');
-    const clickedImageSrc = event.target.dataset.source;
-
-
-
-    const instance = basicLightbox.create(
-        `<img src='${clickedImageSrc}' alt='${clickedImageAlt}'/>`, {
-        onShow: (instance) => {
-            document.addEventListener('keydown', modalClose)
-        },
-        onClose: (instance) => {
-            document.removeEventListener('keydown', modalClose);
-            instance = null;
-        }
-    });
-    const modalClose = (event) => {
-        if (event.key !== 'Escape') {
-            return
-        }
-        return instance.close();
-    }
-
-
-    instance.show();
-}
-
-
-
-
-listEl.addEventListener('click', (event) => {
-    modalOpen(event)
-})
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: "img",
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
